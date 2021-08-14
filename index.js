f588013eec20f3ee1afdbe3be2d1ce2f7dcd08ab
Dimitri Ariadi <dimitri.deren@gmail.com>
2021-08-14 14:26:02 +1200

    added styling and html page

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
+const path = require('path')
+
+app.use(express.static(__dirname + ''))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!') 
+  res.sendFile(path.join(__dirname, '/dashboard.html')); 
 })
 
 app.listen(PORT, () => {
